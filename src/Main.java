143ec9bc3e57f9c67c96fc07ca688b921bf79ac8
syrok32 <stepstepan2@gmail.com>
2024-11-25 19:57:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend % 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = (frog * 10) % 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var massOneBoxer = 78.2;
+        var massTwoBoxer = 82.7;
+        var sum =  massTwoBoxer + massOneBoxer;
+        var difference = massTwoBoxer - massOneBoxer;
+        System.out.println(sum);
+        System.out.println(difference);
+        var division = massTwoBoxer % massOneBoxer;
+        System.out.println(division);
+        var answer = 640 / 8;
+        System.out.println("Всего работников в компании — " + answer + " человек");
+        var answer2 = (answer + 94) * 8;
+        System.out.println("Если в компании работает … человек, то всего " +answer2 + " часов работы может быть поделено между сотрудниками");
+
+
+
+
+
+    }
+}
